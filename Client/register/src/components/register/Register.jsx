3cd88fa630c2aa0dadefc217ed1a79cead0d097f
shutupsuhani <itsscriptsiren@gmail.com>
2023-11-30 19:40:19 +0530

    update

---
+++ Client/register/src/components/register/Register.jsx
@@ -0,0 +1,11 @@
+import "./register.css"
+
+const Register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Register
--- Client/register/src/pages/register/Register.jsx
+++ Client/register/src/pages/register/Register.jsx
@@ -89,4 +89,3 @@ const handleClick = async (e) => {
    
    
    
-
